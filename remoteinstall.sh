d781844ba275c0fbba1cdf4749cc6473b83d4a3a
Rebelllious <macuser@macuser.local>
2015-01-23 22:14:43 +0200

    Update

--- remoteinstall.sh
+++ remoteinstall.sh
@@ -55,7 +55,6 @@ chown -R apache:apache /etc/bar
 #create and populate the php file responsible for communication between the app and the bar installation software
 cat > /var/www/html/install.php <<'CONTENT'
 <?php
-<<<<<<< HEAD
 
 if (preg_match('/;/i', $_REQUEST["device"]) || preg_match('/;/i', $_REQUEST["password"]) || preg_match('/;/i', $_REQUEST["file"])) {
 	die("No way! No malicious actions, please.\n");
@@ -69,15 +68,7 @@ else {
 	echo exec("/etc/bar/install.sh $device $password $file");
 	echo "\n";
 }
-=======
-$device = $_POST['device'];
-$password = $_POST['password'];
-$file = $_POST['file'];
-echo "Installing the app. Please wait a little...";
-echo "\n";
-echo shell_exec("/etc/bar/install.sh $device $password $file");
-echo "\n";
->>>>>>> FETCH_HEAD
+
 ?>
 CONTENT
 
